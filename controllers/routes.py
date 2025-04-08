f08dc5a271360d9169649ee0d3d4c1a298a2c9ce
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-04-08 14:39:23 -0300

    m1

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,1 +1,18 @@
 from flask import render_template, request, redirect, url_for
+
+def init_app(app):
+    # Cria a primeira rota do site
+    @app.route("/")
+    #Cria uma função
+    def home():
+        return render_template('home.html')
+  
+
+    @app.route("/lista")
+    def lista():
+        return render_template('lista.html',)
+    
+
+    @app.route("/cadastroLivro")
+    def cadastroLivro():
+        return render_template('cadastroLivro.html')
