51fdfbe9d000db0e26466ab1338cd17bd1df273a
mtLeme07 <mt.leme07@gmail.com>
2025-04-10 19:30:27 -0300

    Matheus 1

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,1 +1,18 @@
 from flask import render_template, request, redirect, url_for
+
+def init_app(app):
+    # Criando a primeira rota do site
+    @app.route('/')
+    # Criando função no Python
+    def home():
+        return render_template('home.html')
+
+    # Rota de games
+    @app.route('/games')
+    def lista():
+        return render_template('lista.html')
+    
+    # Rota de cadastro de jogos (em dicionário)
+    @app.route('/cadastroLivro')
+    def cadastroLivro():
+        return render_template('cadastroLivro.html')
