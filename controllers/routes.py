a1af49d28fa7f5305dbc7884d8aca9d3e9068c95
mtLeme07 <dev-DanielSilva@outlook.com>
2025-04-14 11:53:16 -0300

    cadastro para lista (M)

--- controllers/routes.py
+++ controllers/routes.py
@@ -3,14 +3,15 @@ from flask import render_template, request, redirect, url_for
 
 listaLivros = [{
             'Titulo' : 'O Pequeno Príncipe',
-            'Publicacao' : 1943,
+            'Publicacao' : "1943-04-20",
             'Autor': 'Antoine de Saint-Exupéry​',
             'Genero': 'Literatura infantojuveni',
             'Editora': 'Agir',
             'Idioma': 'Português Brasileiro',
             'Classificacao': 'Livre',
             'Observacao': 'SimEmbora seja frequentemente catalogado como literatura infantil, O Pequeno Príncipe transcende faixas etárias, oferecendo reflexões profundas sobre temas como amizade, amor, solidão e o sentido da vida. A obra é conhecida por suas metáforas e simbolismos, que permitem múltiplas interpretações e a tornam relevante em diferentes fases da vida. Desde sua publicação, o livro tem encantado leitores ao redor do mundo, sendo traduzido para mais de 160 idiomas e dialetos.',
-        }]
+        },
+               ]
 
 
 def init_app(app):
@@ -57,6 +58,17 @@ def init_app(app):
     # Rota de cadastro de livros (em dicionário)
     @app.route('/cadastroLivro', methods=['GET', 'POST'])
     def cadastroLivro():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('data') and request.form.get('autor'):
+                listaLivros.append({'Titulo' : request.form.get('titulo'),
+                                    'Publicacao' : request.form.get('data'),
+                                    'Autor' : request.form.get('autor'),
+                                    'Genero' : request.form.get('genero'),
+                                    'Editora' : request.form.get('editora'),
+                                    'Idioma' : request.form.get('idioma'),
+                                    'Classificacao' : request.form.get('classificacao'),
+                                    'Observacao' : request.form.get('observacao')})
+            return redirect(url_for('cadastroLivro'))
         return render_template('cadastroLivro.html')
 
     
